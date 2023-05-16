ee2c42f8592fa2a261318ddde8cec7540a27aec0
Saurabh <behl.saurabh@gmail.com>
2023-05-16 12:29:56 +0530

    API Call issue on live

--- app/products/ProductsDipslay.jsx
+++ app/products/ProductsDipslay.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 
+
 const fetchProducts = async (token) => {
   try {
     const res = await fetch('https://nexjdemo.netlify.app/api/products', {
@@ -29,7 +30,7 @@ const ProductsDipslay = async () => {
 					<h4 className="mb-0 font-18">from $ {i.price}</h4>
 				</div>
 				<div className="text-end pe-0">
-					<button className="sky-blue-button  select-box border-0">Select</button>
+					<button className="sky-blue-button select-box border-0">Select</button>
 				</div>
 			</div>
 		))}
